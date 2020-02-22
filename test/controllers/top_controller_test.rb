7087fd08cd6de61f3d44fe11ccecf06872db8e7a
石渡拓海 <ishiwatatakumi@ishiwatatakuminoMacBook-Pro.local>
2020-02-22 19:49:40 +0900

    improve test +

--- test/controllers/top_controller_test.rb
+++ test/controllers/top_controller_test.rb
@@ -2,7 +2,7 @@ require 'test_helper'
 
 class TopControllerTest < ActionDispatch::IntegrationTest
   test "should get index" do
-    get root
+    get root_url
     assert_response :success
   end
 
