0ebf3625b6383a60af17b60a13857ba5d61f0d14
石渡拓海 <ishiwatatakumi@ishiwatatakuminoMacBook-Pro.local>
2020-02-22 19:25:51 +0900

    initial commit

---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+  
+  root to: 'top#index'
+  resources :health_check, only: [:index]
+
+  # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
+end
